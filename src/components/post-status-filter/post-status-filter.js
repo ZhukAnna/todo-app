ffd2c5112ef8dc4947391c43cbc768f62bcc6d00
Anna Zhukova <anna.zhukanna@gmail.com>
2021-02-04 12:25:57 +0500

    core

---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PostFilter = () => {
+    return (
+        <div className="btn-group">
+            <button>
+                1
+            </button>
+            <button>
+                2
+            </button>
+        </div>
+    )
+}
+
+export default PostFilter;
