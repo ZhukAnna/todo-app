307525fc4a28214ba958a7a80d145bd5ba81fa20
Anna Zhukova <anna.zhukanna@gmail.com>
2021-06-09 01:37:42 +0500

    Формы

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,12 +13,21 @@ export default class App extends Component {
         super(props);
         this.state = {
             data: [
-                { label: 'Going to eat', important: true, id: 'dkcs' },
-                { label: 'Sleep', important: false, id: 'fofs' },
-                { label: 'Play in football', important: false, id: 'ffyk' }
-            ]
+                { label: 'Going to eat', important: true, like: false, id: 1 },
+                { label: 'Sleep', important: false, like: false, id: 2 },
+                { label: 'Play in football', important: false, like: false, id: 3 }
+            ],
+            term: '',
+            filter: 'all'
         };
         this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+        this.onToggleImportant = this.onToggleImportant.bind(this);
+        this.onToggleLiked = this.onToggleLiked.bind(this);
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
+        this.onFilterSelect = this.onFilterSelect.bind(this);
+
+        this.maxId = 4;
     }
 
     deleteItem(id) {
@@ -34,16 +43,93 @@ export default class App extends Component {
         });
     }
 
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({ data }) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            };
+        });
+    }
+
+    onToggleImportant(id) {
+        this.setState(({ data }) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const old = data[index];
+            const newItem = { ...old, important: !old.important };
+            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+            return {
+                data: newArr
+            };
+        });
+    }
+
+    onToggleLiked(id) {
+        this.setState(({ data }) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const old = data[index];
+            const newItem = { ...old, like: !old.like };
+            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+            return {
+                data: newArr
+            };
+        });
+    }
+
+    searchPost(items, term) {
+        if (term.length === 0) {
+            return items
+        }
+        return items.filter((item) => {
+            return item.label.indexOf(term) > -1
+        })
+    }
+
+    filterPost(items, filter) {
+        if (filter === 'like') {
+            return items.filter(item => item.like)
+        } else {
+            return items
+        }
+    }
+
+    onUpdateSearch(term) {
+        this.setState({ term });
+    }
+
+    onFilterSelect(filter) {
+        this.setState({ filter });
+    }
+
     render() {
+        const { data, term, filter } = this.state;
+        const liked = data.filter(item => item.like).length;
+        const allPosts = data.length;
+        const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
+
         return (
             <div className="app" >
-                <AppHeader />
+                <AppHeader
+                    liked={liked}
+                    allPosts={allPosts} />
                 <div className="search-panel d-flex">
-                    <SearchPanel />
-                    <PostFilter />
+                    <SearchPanel
+                        onUpdateSearch={this.onUpdateSearch} />
+                    <PostFilter
+                        filter={filter}
+                        onFilterSelect={this.onFilterSelect} />
                 </div>
-                <PostList posts={this.state.data} onDelete={this.deleteItem} />
-                <PostAddForm />
+                <PostList
+                    posts={visiblePosts}
+                    onDelete={this.deleteItem}
+                    onToggleImportant={this.onToggleImportant}
+                    onToggleLiked={this.onToggleLiked} />
+                <PostAddForm onAdd={this.addItem} />
             </div>
         )
     }
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -3,32 +3,10 @@ import '../post-list-item/post-list-item.css';
 
 export default class PostListItem extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            important: false,
-            like: false
-        };
-        this.onImportant = this.onImportant.bind(this);
-        this.onLike = this.onLike.bind(this);
-    }
-
-    onImportant() {
-        this.setState(({ important }) => ({
-            important: !important
-        }))
-    }
-
-    onLike() {
-        this.setState(({ like }) => ({
-            like: !like
-        }))
-    }
 
     render() {
 
-        const { label, onDelete } = this.props;
-        const { important, like } = this.state;
+        const { label, onDelete, important, like, onToggleImportant, onToggleLiked } = this.props;
         let classes = "app-list-item d-flex justify-content-between";
 
         if (important) {
@@ -41,11 +19,11 @@ export default class PostListItem extends Component {
 
         return (
             <div className={classes}>
-                <span className="app-list-item__label" onClick={this.onLike}>
+                <span className="app-list-item__label" onClick={onToggleLiked}>
                     {label}
                 </span>
                 <div className="d-flex justify-content-center align-items-center">
-                    <button type="button" className="btn-star btn-sm" onClick={this.onImportant}>
+                    <button type="button" className="btn-star btn-sm" onClick={onToggleImportant}>
                         <i className="fa fa-star"></i>
                     </button>
                     <button type="button" className="btn-trash btn-sm" onClick={onDelete}>
